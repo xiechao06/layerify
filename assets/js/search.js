4e0515be175f25f1d2f3e36eee4c234b30c3ab80
xiechao <xiechao06>
2019-03-22 19:40:58 +0800

    Updates

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"layerifyObj","url":"globals.html#layerifyobj","classes":"tsd-kind-function tsd-is-not-exported"},{"id":1,"kind":64,"name":"layerify","url":"globals.html#layerify","classes":"tsd-kind-function"}]};
